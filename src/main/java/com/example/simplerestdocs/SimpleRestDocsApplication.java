c7ebb6428c9406215adb97910d669c6482e96cb4
Syl8n <magi8520@gmail.com>
2023-05-15 16:05:55 +0900

    Initial commit

---
+++ src/main/java/com/example/simplerestdocs/SimpleRestDocsApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplerestdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRestDocsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleRestDocsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simplerestdocs/SimpleRestDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplerestdocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRestDocsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
